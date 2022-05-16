0287720c41852eb08020f503fe40ba823d02fc9b
StitZle <official@niclasbuerger.de>
2022-05-16 23:34:59 +0200

    FHS/change-postgres-to-mongo - removed store from frontend

--- fire-hose-management-backend/src/main/java/com/niclas/model/Device.java
+++ fire-hose-management-backend/src/main/java/com/niclas/model/Device.java
@@ -21,39 +21,48 @@ public class Device extends AuditModel {
     public Device() {
     }
 
+
     public Device( String deviceName, String deviceId, boolean isPrimary ) {
         this.deviceName = deviceName;
         this.deviceId = deviceId;
         this.isPrimary = isPrimary;
     }
 
+
     public String getId() {
         return id.toHexString();
     }
+
 
     public void setId( ObjectId id ) {
         this.id = id;
     }
 
+
     public String getDeviceName() {
         return deviceName;
     }
+
 
     public void setDeviceName( String component ) {
         this.deviceName = component;
     }
 
+
     public String getDeviceId() {
         return deviceId;
     }
+
 
     public void setDeviceId( String deviceId ) {
         this.deviceId = deviceId;
     }
 
+
     public boolean getIsPrimary() {
         return isPrimary;
     }
+
 
     public void setIsPrimary( boolean primary ) {
         this.isPrimary = primary;
